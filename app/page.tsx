8b3602dea57462e98809f06f6517d04b4720630d
Sherry <sherrygong2@gmail.com>
2024-12-30 11:56:35 -0600

    github pages

--- app/page.tsx
+++ app/page.tsx
@@ -133,7 +133,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
         <button
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            onClick={() => setIsAlarmSet(true)}
+            onClick={() => [setIsAlarmSet(true), setBackgroundColor("0, 0, 0")]}
           >
             Set sunrise alarm now
           </button>
--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,11 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  basePath: "/sunrise",
+  output: 'export',
   images: {
     unoptimized: true,
-  },
-  assetPrefix: isProd ? '/sunrise/' : '',
-  basePath: isProd ? '/sunrise' : '',
-  output: 'export'
+  }
 };
 
 export default nextConfig;
